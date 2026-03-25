8588d6c39ac1d4641939cb303e376315b4683a1b
liujiahua <1598814057@qq.com>
2026-03-25 11:53:33 +0800

    禁用了redis，问题还未解决，应该是3.15自身问题，添加了gitignore

--- src/main/java/org/xinrui/Application.java
+++ src/main/java/org/xinrui/Application.java
@@ -3,10 +3,19 @@ package org.xinrui;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.redis.RedisReactiveAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.redis.RedisRepositoriesAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        DataSourceAutoConfiguration.class,
+        RedisAutoConfiguration.class,
+        RedisRepositoriesAutoConfiguration.class,
+        RedisReactiveAutoConfiguration.class
+})
 public class Application {
 
     public static void main(String[] args) {
